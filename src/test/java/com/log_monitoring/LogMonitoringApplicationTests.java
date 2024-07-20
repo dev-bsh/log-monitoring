e32bf481f970338985e598a39b87c6f1dd9abe6c
BSH <gamaugi11@gmail.com>
2024-07-21 06:50:27 +0900

    init

---
+++ src/test/java/com/log_monitoring/LogMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.log_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
